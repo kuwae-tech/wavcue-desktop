d53b80af2e145bf1588a3e438fc3524cb7d2832c
kuwae-tech <kuwae2419@gmail.com>
2026-02-04 09:32:00 +0900

    Raise min width and restore badge position

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: initialWidth,
     height: initialHeight,
-    minWidth: 1320,
+    minWidth: 1590,
     minHeight: 680,
     autoHideMenuBar: true,
     backgroundColor: '#0b0c0f',
@@ -162,7 +162,7 @@ const createWindow = () => {
   });
 
   mainWindow.loadFile(path.join(__dirname, 'renderer', 'prototype.html'));
-  mainWindow.setMinimumSize(1320, 680);
+  mainWindow.setMinimumSize(1590, 680);
 
   if (!isMac) {
     mainWindow.setMenuBarVisibility(false);
